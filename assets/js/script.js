0c33043b3bb4ffb190531c71eb5d108bf3496e8d
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-07-02 21:37:09 +0100

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Custom JavaScript code
+
+// Example function
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+// Example event listener
+document.addEventListener('DOMContentLoaded', function() {
+  // Code to run when the DOM is ready
+  greet('John');
+});
+
+// Other JavaScript code...
